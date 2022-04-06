9007cb94c7baefbe1595293964b4f88906edbb7c
Nushrat Jahan <sadianushrat7@gmail.com>
2022-04-06 18:09:44 +0800

    First commit<Hello, I'm Nushrat>

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  var firstname  = "nushrat";
+  dynamic lastname ="sadia";
+  
+  int birthyear = 1999;
+  int age = 2022 - birthyear;
+  
+  double cgpa = 3.5;
+  String address = "Aminah";
+  
+  print("My name is $firstname $lastname. I am $age this year. I am staying at $address. My CGPA is $cgpa.");
+}
